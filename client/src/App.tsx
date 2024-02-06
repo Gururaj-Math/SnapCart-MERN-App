13ab5a699890fe1762717d8b7fa5dcbaa71e823f
Gururaj <gururajmath3@gmail.com>
2024-02-06 15:21:25 +0530

    tailwindcss initialize

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,19 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import register from './pages/auth/forms/register';
-import login from './pages/auth/forms/login';
+import register from "./pages/auth/forms/register";
+import login from "./pages/auth/forms/login";
 
 function App() {
- 
-
   return (
     <>
-     <Router>
+      <Router>
         <Routes>
           <Route path="auth/login" Component={login}></Route>
           <Route path="auth/register" Component={register}></Route>
         </Routes>
-     </Router>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
