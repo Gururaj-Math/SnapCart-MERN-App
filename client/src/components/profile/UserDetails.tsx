89258f0bf8c2a5f23046cc24df4e6fa708b83643
Gururaj <gururajmath3@gmail.com>
2024-02-09 00:25:43 +0530

    user details ui update

--- client/src/components/profile/UserDetails.tsx
+++ client/src/components/profile/UserDetails.tsx
@@ -1,8 +1,37 @@
 import { Avatar } from 'antd';
-import Card from 'antd/es/card/Card';
+import { Card, Space } from 'antd';
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  InstagramOutlined,
+  LinkedinOutlined,
+  CodepenOutlined,
+  GithubOutlined,
+  MediumOutlined,
+} from '@ant-design/icons';
+import React from 'react';
+
 const { Meta } = Card;
 
+const socialMediaIcons: { [key: string]: JSX.Element } = {
+  facebook: <FacebookOutlined />,
+  twitter: <TwitterOutlined />,
+  instagram: <InstagramOutlined />,
+  linkedin: <LinkedinOutlined />,
+  codepen: <CodepenOutlined />,
+  github: <GithubOutlined />,
+  medium: <MediumOutlined />,
+};
+
 const UserDetails = (props: { currentUser: any; userPosts: String[] }) => {
+  const renderSocialMediaIcon = (link: string) => {
+    const socialMediaPlatforms: string[] = Object.keys(socialMediaIcons);
+    const platform = socialMediaPlatforms.find((platform) => link.includes(platform));
+    const icon = platform ? socialMediaIcons[platform] : null;
+    const iconStyle = { fontSize: '24px' };
+    return icon ? React.cloneElement(icon, { style: iconStyle }) : null;
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <Meta
@@ -24,29 +53,30 @@ const UserDetails = (props: { currentUser: any; userPosts: String[] }) => {
           <span className="text-gray-400">Following</span>{' '}
         </p>
       </div>
-      <div className="p-4 border-2 rounded-md flex justify-between px-8">
+      <div className="p-4 border-2 rounded-md flex justify-between px-12">
         <div>
           <h1 className="font-semibold text-gray-500">Bio</h1>
           <p>{props.currentUser.bio}</p>
           <h1 className="font-semibold text-gray-500">Location</h1>
           <p>{props.currentUser.location}</p>
         </div>
-        <div>
-          <h1 className="font-semibold text-gray-500">Social Links</h1>
-          <div className="flex flex-col">
-            {props.currentUser.links.map((link: string, index: number) => (
-              <a href={link} key={index} target="_blank" rel="noopener noreferrer">
-                {link}
-              </a>
-            ))}
+        <div className="flex flex-col justify-center items-center px-4 gap-2">
+          <h1 className="font-semibold text-gray-500 text-center">Social Links</h1>
+          <div className="flex items-center gap-4">
+            {props.currentUser.links
+              .filter((link: string) => link && typeof link === 'string')
+              .map((link: string, index: number) => (
+                <a href={link} key={index} target="_blank" rel="noopener noreferrer">
+                  {renderSocialMediaIcon(link)}
+                </a>
+              ))}
           </div>
         </div>
       </div>
-      <div className="flex w-full gap-4 flex-wrap justify-evenly max-h-[200px] overflow-auto">
+      <div className="flex w-full gap-2 flex-wrap justify-evenly max-h-[200px] overflow-auto">
         {props.userPosts.map((post: any, index) => (
           <div key={index} className="border-2 rounded-md">
-            <p className="p-2 border-b-2 font-semibold">{post.title}</p>
-            <img src={post.image} className="w-[150px] h-[150px] rounded-b-md" />
+            <img src={post.image} className="w-[200px] h-[200px] rounded-md" />
           </div>
         ))}
       </div>
